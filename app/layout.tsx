73b5b1e9a995dd940ef3cf98199c80a6fb1364a3
Mo Adel <adel12790@gmail.com>
2025-11-28 23:48:27 +0100

    feat: add vercel analytics lib

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Mohamed Adel - AI Resume",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
         <SpeedInsights />
+        <Analytics />
         {children}
       </body>
     </html>
